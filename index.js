d4f2b66d9cb00ad02d20a41d8baa553d161e47d4
Sarah <sarahbezerra2013@gmail.com>
2022-01-24 14:03:43 -0300

    post tweets

--- index.js
+++ index.js
@@ -6,9 +6,17 @@ server.use(express.json());
 server.use(cors());
 
 let user = [];
+const tweets = [];
 
 server.post('/sign-up', (req, res) => {
     user = req.body;
+    res.send("OK");
+});
+
+server.post('/tweets', (req, res) => {
+    const tweet = req.body;
+    tweet.avatar = user.avatar;
+    tweets.push(tweet);
     res.send("OK");
 });
 
